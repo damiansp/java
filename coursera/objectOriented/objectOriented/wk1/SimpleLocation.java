a2a0c0b6e23442d3253136990826008f10d9835e
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-04-10 20:11:19 -0700

    init coursera oop coruse with oop/wk1

---
+++ coursera/objectOriented/objectOriented/wk1/SimpleLocation.java
@@ -0,0 +1,13 @@
+public class SimpleLocation {
+  public double latitude;
+  public double longitude;
+
+  public SimpleLocation(double lat, double lon) {
+    this.latitude = lat;
+    this.longitude = lon;
+  }
+
+  public double distance(SimpleLocation other) {
+    /* ... */
+  }
+}
